271fa371478f36de1dfc93b7b647f8001d1e9c19
Cocoiscool-531 <119710842+Cocoiscool-531@users.noreply.github.com>
2024-10-13 01:42:41 -0400

    Add "ChamberOnly" Auto Paths (need tweaking) Add @Autonomous on all auto modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueLeftSelfish.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/BlueLeftSelfish.java
@@ -2,11 +2,13 @@ package org.firstinspires.ftc.teamcode.auto;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
+@Autonomous
 public class BlueLeftSelfish extends LinearOpMode {
 
     @Override
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedLeftChamberOnly.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
+
+@Autonomous
+public class RedLeftChamberOnly extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+
+        TrajectorySequence path = drive.trajectorySequenceBuilder(new Pose2d(-12, 65.5, Math.toRadians(-90)))
+                .waitSeconds(10)
+                .splineTo(new Vector2d(8, -30), Math.toRadians(90))
+                .waitSeconds(3)
+                .back(12)
+                .splineTo(new Vector2d(60, -60), Math.toRadians(0))
+                .build();
+
+        waitForStart();
+
+        drive.followTrajectorySequence(path);
+    }
+}
