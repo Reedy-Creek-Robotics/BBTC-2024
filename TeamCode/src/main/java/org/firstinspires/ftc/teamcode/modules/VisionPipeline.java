109d785f9a9942f8182785ba77830a6af41b9a44
Daniel <danielns@gmail.com>
2025-01-27 18:14:03 -0500

    fixing some calculations in  VisionPipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/VisionPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/VisionPipeline.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.modules;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 
+import com.google.blocks.ftcrobotcontroller.runtime.obsolete.TfodCustomModelAccess;
+
 import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
 import org.firstinspires.ftc.vision.VisionProcessor;
 import org.opencv.core.Core;
@@ -21,6 +23,7 @@ import java.util.List;
 
 
 public class VisionPipeline implements VisionProcessor {
+
     final double width = 1920;
     final double height = 1080;
     final double screenCenterX = width / 2;
@@ -83,7 +86,6 @@ public class VisionPipeline implements VisionProcessor {
     public Object processFrame(Mat frame, long captureTimeNanos) {
 
         editingFrame = frame;
-        //Calib3d.undistort(frame,editingFrame,cameraMatrix,distCoeffs);
         long startTime = System.nanoTime();
         List<List<Object>> samplesData = new ArrayList<>();
         preprocess(editingFrame);
@@ -245,12 +247,10 @@ public class VisionPipeline implements VisionProcessor {
             xAngle = -xAngle;
         }
         double yRealWorld = distanceOffGround / Math.tan(Math.toRadians(yAngleDown));
-        double xRealWorld = Math.tan(xAngle) * yRealWorld;
+        double xRealWorld = Math.tan(Math.toRadians(xAngle)) * yRealWorld;
         if (xIsNegative)
             xRealWorld = -xRealWorld;
         return  new Point(xRealWorld, yRealWorld);
-
-
     }
 
     private Point transformPosition(Point robot, double degreesR, Point offset) {
